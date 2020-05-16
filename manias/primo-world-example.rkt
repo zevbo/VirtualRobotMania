079755c5107c6d61a93c48813a17ced76d336181
zevbo <minsky-pz@bxscience.edu>
2020-05-16 14:11:05 -0400

    oops small change forgot to save

--- manias/primo-world-example.rkt
+++ manias/primo-world-example.rkt
@@ -2,6 +2,12 @@
 (require "../lib/worlds/primo-world.rkt")
 
 ;; The width of the field is 700 and the height is 500
+
+;; BASICS
+;; (if trueOrFalse then else)
+;; (+ nums ...), (- num1 num2), (* nums ...), (/ num1 num2)
+;; (< num1 num2), (= num1 num2), (> num1 num2)
+
 ;; STUFF TO TRY
 ;; (set-motors! left% right%) -> set's torque on wheels (max = 1)
 ;; (change-motor-inputs Δleft% Δright%) -> changes wheel torque by the delta
@@ -10,11 +16,6 @@
 ;; (get-looking-dist angle) -> angle is in degrees, get's dist from center of bot
 ;; (random) -> get's a random number between 0 and 1
 ;; (random num1 num2) -> get's a random integer between num1 and num2 - 1
-
-;; BASICS
-;; (if trueOrFalse then else)
-;; (+ nums ...), (- num1 num2), (* nums ...), (/ num1 num2)
-;; (< num1 num2), (= num1 num2), (> num1 num2)
 
 ;; MAKE ROBOT OPTIONS
 ;; #:image-url, #:name-color, #:name-font, #:body-color, #:wheel-color
