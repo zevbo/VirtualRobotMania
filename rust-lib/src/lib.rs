f84f7269e66f89f5d75c41c2d2e1bd8e20165c3b
Yaron M. Minsky <yminsky@gmail.com>
2020-06-20 15:32:27 -0400

    basic use of nalgebra package

--- rust-lib/src/lib.rs
+++ rust-lib/src/lib.rs
@@ -1,7 +1,13 @@
+extern crate nalgebra as na;
+//use na::Vector2;
+
 #[cfg(test)]
 mod tests {
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        let mut x = na::Vector2::new(3.3, 0.3);
+        let y = na::Vector2::new(3.3, 2.);
+        x = na::Vector2::new(x[0], 2.);
+        assert_eq!(x, y);
     }
 }
