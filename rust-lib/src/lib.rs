61e8b701fb18e79cddec9f921da70d3177fb76e6
Yaron M. Minsky <yminsky@gmail.com>
2020-06-20 15:35:05 -0400

    small tweak

--- rust-lib/src/lib.rs
+++ rust-lib/src/lib.rs
@@ -1,5 +1,5 @@
 extern crate nalgebra as na;
-//use na::Vector2;
+//use nalgebra::Vector2;
 
 #[cfg(test)]
 mod tests {
@@ -7,7 +7,7 @@ mod tests {
     fn it_works() {
         let mut x = na::Vector2::new(3.3, 0.3);
         let y = na::Vector2::new(3.3, 2.);
-        x = na::Vector2::new(x[0], 2.);
+        x[1] = 2.0;
         assert_eq!(x, y);
     }
 }
