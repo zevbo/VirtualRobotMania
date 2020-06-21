cd6da47947e967f95f9eba9e3b9a355602347a9d
Yaron M. Minsky <yminsky@gmail.com>
2020-06-21 08:47:53 -0400

    Added very basic call from Racket to Rust

---
+++ rust-lib/include/rust_mania.h
@@ -0,0 +1,10 @@
+#include <cstdarg>
+#include <cstdint>
+#include <cstdlib>
+#include <new>
+
+extern "C" {
+
+int32_t fnurk(int32_t x, int32_t y);
+
+} // extern "C"
---
+++ rust-lib/includegen.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+cbindgen > include/rust_mania.h
