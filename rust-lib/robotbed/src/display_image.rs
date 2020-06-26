81fbbcf3a1ff1b5ea2f6d03df217355978d47ae3
zevbo <minsky-pz@bxscience.edu>
2020-06-26 03:28:15 -0400

    aight I lied about going to sleep. Had to make displayer publico

--- rust-lib/robotbed/src/display_image.rs
+++ rust-lib/robotbed/src/display_image.rs
@@ -5,30 +5,12 @@ use core::ops::Deref;
 
 // Trying to change this so that the second ImageBuffer type can be anything that impl: Deref<Target = [Rgb<u8>::Subpixel]
 //    but it says "ambiguous assosicated type"
+
 pub fn display_image<Container: Deref<Target = [u8]>>(img_buf : ImageBuffer<Rgb<u8>,Container>){
     displayer(|| {img_buf});
 }
 
-fn collapse_rgb(rgb : &Rgb<u8>) -> u32 {
-    let r = rgb[0] as u32;
-    let g = rgb[1] as u32;
-    let b = rgb[2] as u32;
-    return (r << 16) | (g << 8) | b;
-}
-
-fn image_buffer_to_buffer<Container: Deref<Target = [u8]>>(img_buf : ImageBuffer<Rgb<u8>, Container>) -> (Vec<u32>, usize, usize){
-    let image_width  = img_buf.width()  as usize;
-    let image_height = img_buf.height() as usize;
-    let pixels = img_buf.pixels();
-    let mut buffer = Vec::new();
-    for pixel in pixels{
-        buffer.push(collapse_rgb(&pixel.to_rgb()));
-    }
-    return (buffer, image_width, image_height);
-
-}
-
-fn displayer<Container: Deref<Target = [u8]>, F>(get_img: F) where F: FnOnce() -> ImageBuffer<Rgb<u8>,Container> {
+pub fn displayer<Container: Deref<Target = [u8]>, F>(get_img: F) where F: FnOnce() -> ImageBuffer<Rgb<u8>,Container> {
 
     let (buffer, image_width, image_height) = image_buffer_to_buffer(get_img());
 
@@ -52,4 +34,23 @@ fn displayer<Container: Deref<Target = [u8]>, F>(get_img: F) where F: FnOnce() -
             .update_with_buffer(&buffer, image_width, image_height)
             .unwrap();
     }
+}
+
+fn collapse_rgb(rgb : &Rgb<u8>) -> u32 {
+    let r = rgb[0] as u32;
+    let g = rgb[1] as u32;
+    let b = rgb[2] as u32;
+    return (r << 16) | (g << 8) | b;
+}
+
+fn image_buffer_to_buffer<Container: Deref<Target = [u8]>>(img_buf : ImageBuffer<Rgb<u8>, Container>) -> (Vec<u32>, usize, usize){
+    let image_width  = img_buf.width()  as usize;
+    let image_height = img_buf.height() as usize;
+    let pixels = img_buf.pixels();
+    let mut buffer = Vec::new();
+    for pixel in pixels{
+        buffer.push(collapse_rgb(&pixel.to_rgb()));
+    }
+    return (buffer, image_width, image_height);
+
 }
