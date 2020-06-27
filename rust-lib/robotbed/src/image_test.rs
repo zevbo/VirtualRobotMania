4c82679a90a0017f320137e06c1fb80364e355c2
Yaron M. Minsky <yminsky@gmail.com>
2020-06-27 13:30:59 -0400

    make test more interesting

--- rust-lib/robotbed/src/image_test.rs
+++ rust-lib/robotbed/src/image_test.rs
@@ -4,26 +4,29 @@ use crate::display_image;
 use crate::image_helpers::{download_img, rotate_overlay, scale_down};
 use image::ImageBuffer;
 
-const WIDTH: u32 = 600;
-const HEIGHT: u32 = 600;
+const WIDTH: u32 = 1200;
+const HEIGHT: u32 = 1200;
 
 pub fn display_img() {
     // a default (black) image containing Rgb values
     println!("downloading image...");
     let on_top_big = download_img("../pelosi.jpeg");
     println!("downloaded!");
-    let on_top = scale_down(on_top_big, 0.1);
+    let on_top = scale_down(on_top_big, 0.3);
     println!("scaled!");
     let mut imgbuf = ImageBuffer::new(WIDTH, HEIGHT);
     println!("new imbuf");
     //image::imageops::overlay(&mut imgbuf, &on_top, 400, 120);
-    let mut tester = || rotate_overlay(&mut imgbuf, &on_top, 400, 120, 0.1 * core::f32::consts::PI);
-    tester();
-    tester();
-    tester();
-    tester();
-    tester();
-    tester();
+    let mut tester =
+        |x: f32| rotate_overlay(&mut imgbuf, &on_top, 400, 120, x * core::f32::consts::PI);
+    let mut count = 0.;
+    loop {
+        count = count + 0.05;
+        tester(count);
+        if count > 1. {
+            break;
+        }
+    }
     println!("overlayed");
     display_image::display_static_image(imgbuf);
 }
