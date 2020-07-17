edb2ceeda88cdc0db791125ff7162d2891f3febf
zevbo <minsky-pz@bxscience.edu>
2020-07-17 15:11:54 -0400

    pelos-mobeel can now chase

--- lib/worlds/dodgeball-world/example-bots/pelosi-mobeel.rkt
+++ lib/worlds/dodgeball-world/example-bots/pelosi-mobeel.rkt
@@ -3,24 +3,34 @@
 (provide pelosi-mobeel)
 
 (define p 0.01)
+(define neut-ball-p .012)
 (define (on-tick tick#)
   (set-degree-mode)  ;; make sure to have this line in on-tick
   (define angle (angle-to-other-bot))
   (cond:
     [(not (or (front-left-close?) (front-right-close?)))  (set-motors -1.5 1)]
     [(or (back-left-close?) (back-right-close?))  (set-motors 1.5 -1)]
-    [(not
-      (and
+    [(> (num-balls-left) 0)
+     (define angles (angles-to-neutral-balls))
+     (cond
+      [(empty? angles) (set-motors! 0 0)]
+      [else
+       (-*-
+        (foldl goal-angle 
+            (define (lambda (a1 a2) (if (> (get-looking-dist a1) (get-looking-dist a2)) a2 a1))
+                    (first angles) (rest angles))))
+       (set-motors! (- 1 (* neut-ball-p goal-angle)) (+ 1 (* neut-ball-p goal-angle)))])]
+    [(and
       (or
        (and (< (abs angle) 7) (< (dist-to-other-bot) 300))
        (and (< (abs angle) 30) (< (dist-to-other-bot) 100))
        (and (< (abs angle) 60) (< (dist-to-other-bot) 60)))
-      (or true (< (get-cooldown-time) 0))))
+      (or true (< (get-cooldown-time) 0)))
      (set-motors! 1 1)
      (shoot)]
-    [(> (get-cooldown-time) 0) (set_motors! 2 0.3)]
-    [(> (dist-to-other-bot) 125) (if (> (abs angle) 90) (set-motors! 1 0.8) (set-motors! -1 -0.8))]
-    [#t (set_motors! (- (* 2 p angle) 2) (+ 2 (* p angle)))] 
+    [(<= (get-cooldown-time) 0) (set_motors! 2 0.3)]
+    [(< (dist-to-other-bot) 125) (if (> (abs angle) 90) (set-motors! 1 0.8) (set-motors! -1 -0.8))]
+    [#f (set_motors! (- (* 2 p angle) 2) (+ 2 (* p angle)))] 
     )
   )
 
