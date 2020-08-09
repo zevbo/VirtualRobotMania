d82927083dc318a42363e1613f6b8d954a929fc2
Yaron M. Minsky <yminsky@gmail.com>
2020-08-09 14:05:36 -0400

    tweaking tests

--- ocaml/geo/test/test_seg.ml
+++ ocaml/geo/test/test_seg.ml
@@ -1,7 +1,7 @@
 open! Core_kernel
 open Geo
 
-let v = Vec.create
+let v x y = Vec.create (Int.to_float x) (Int.to_float y)
 let s = Line_seg.create
 
 let%expect_test "create and show seg" =
@@ -10,8 +10,20 @@ let%expect_test "create and show seg" =
     let ll = Line_seg.to_ll seg in
     print_s [%message "" (seg : Line_seg.t) (ll : Line_like.t)]
   in
-  show (v 1. 1.) (v 3. 3.);
+  show (v 1 1) (v 3 3);
   [%expect
     {|
     ((seg ((pt1 (1 1)) (pt2 (3 3))))
      (ll ((base (2 2)) (dir_vec (-2 -2)) (flips (0.5 -0.5))))) |}]
+
+let%expect_test "intersection" =
+  let intersect s1 s2 =
+    print_s
+      [%sexp
+        (Line_like.intersection (Line_seg.to_ll s1) (Line_seg.to_ll s2)
+          : Vec.t option)]
+  in
+  intersect (s (v (-1) (-1)) (v 1 1)) (s (v (-1) 1) (v 1 (-1)));
+  [%expect {| ((0 0)) |}];
+  intersect (s (v 0 4) (v 4 0)) (s (v 4 4) (v 0 1));
+  [%expect {| ((1.7142857 2.2857143)) |}]
