7519c65c0dc8b49dfac470e6e67e13a3df404720
Yaron M. Minsky <yminsky@gmail.com>
2020-08-09 14:29:39 -0400

    more tests

--- ocaml/geo/test/test_seg.ml
+++ ocaml/geo/test/test_seg.ml
@@ -2,11 +2,12 @@ open! Core_kernel
 open Geo
 
 let v x y = Vec.create (Int.to_float x) (Int.to_float y)
-let s = Line_seg.create
+let s a b = Line_seg.to_ll (Line_seg.create a b)
+let r a b = Ray.to_ll (Ray.create a b)
 
 let%expect_test "create and show seg" =
   let show v1 v2 =
-    let seg = s v1 v2 in
+    let seg = Line_seg.create v1 v2 in
     let ll = Line_seg.to_ll seg in
     print_s [%message "" (seg : Line_seg.t) (ll : Line_like.t)]
   in
@@ -17,11 +18,8 @@ let%expect_test "create and show seg" =
      (ll ((base (2 2)) (dir_vec (-2 -2)) (flips (0.5 -0.5))))) |}]
 
 let%expect_test "intersection" =
-  let intersect s1 s2 =
-    print_s
-      [%sexp
-        (Line_like.intersection (Line_seg.to_ll s1) (Line_seg.to_ll s2)
-          : Vec.t option)]
+  let intersect ll1 ll2 =
+    print_s [%sexp (Line_like.intersection ll1 ll2 : Vec.t option)]
   in
   intersect (s (v (-1) (-1)) (v 1 1)) (s (v (-1) 1) (v 1 (-1)));
   [%expect {| ((0 0)) |}];
@@ -31,7 +29,14 @@ let%expect_test "intersection" =
   [%expect {| () |}];
   intersect (s (v 0 0) (v 6 6)) (s (v 10 0) (v 0 10));
   [%expect {| ((5 5)) |}];
-  intersect (s (v 0 0) (v 5 5)) (s (v 10 0) (v 0 10));
+  intersect (s (v 0 0) (Vec.create 5.01 5.01)) (s (v 10 0) (v 0 10));
+  [%expect {| ((5 5)) |}];
+  intersect (s (v 0 0) (Vec.create 5. 5.)) (s (v 10 0) (v 0 10));
   (* Mildly surprising that this doesn't cause an intersection, 
      but not a big deal *)
-  [%expect {| () |}]
+  [%expect {| () |}];
+  (* We can also intersect a ray and a segment. *)
+  intersect (s (v 10 0) (v 0 10)) (r (v 0 0) (v 1 1));
+  [%expect {| ((5 5)) |}];
+  intersect (s (v 10 0) (v 0 10)) (r (v 0 0) (v 1 2));
+  [%expect {| ((3.3333333 6.6666667)) |}]
