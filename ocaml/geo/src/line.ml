b9d1f19e879d6dbb3b85f64a8d288acf74b42e97
Yaron M. Minsky <yminsky@gmail.com>
2020-08-09 13:55:46 -0400

    whitespace

--- ocaml/geo/src/line.ml
+++ ocaml/geo/src/line.ml
@@ -1,12 +1,13 @@
-type t = {pt1: Vec.t; pt2: Vec.t} [@@deriving sexp]
+type t =
+  { pt1 : Vec.t
+  ; pt2 : Vec.t
+  }
+[@@deriving sexp]
 
-let create pt1 pt2 = {pt1; pt2}
-
-let to_ll t =
-  Line_like.create_w_flip_points t.pt1 (Vec.sub t.pt2 t.pt1) []
+let create pt1 pt2 = { pt1; pt2 }
+let to_ll t = Line_like.create_w_flip_points t.pt1 (Vec.sub t.pt2 t.pt1) []
 
 let point_angle_form pt angle =
   create pt (Vec.add (Vec.rotate (Vec.create 1. 0.) angle) pt)
 
-let point_slope_form pt slope =
-  point_angle_form pt (Float.atan slope)
+let point_slope_form pt slope = point_angle_form pt (Float.atan slope)
--- ocaml/geo/src/square.ml
+++ ocaml/geo/src/square.ml
@@ -1,14 +1,24 @@
 open! Vec
 open! Base
 
-type t = {width: float; height: float; center: Vec.t}
+type t =
+  { width : float
+  ; height : float
+  ; center : Vec.t
+  }
 
 let contains t pt =
-  General.imp_equals pt.x t.center.x ~epsilon:(t.width /. 2.) &&
-  General.imp_equals pt.y t.center.y ~epsilon:(t.height /. 2.)
+  General.imp_equals pt.x t.center.x ~epsilon:(t.width /. 2.)
+  && General.imp_equals pt.y t.center.y ~epsilon:(t.height /. 2.)
 
-let get_corners t = 
-  let w = t.width /. 2. in 
+let get_corners t =
+  let w = t.width /. 2. in
   let h = t.height /. 2. in
-  let relative_corners =  [Vec.create w h; Vec.create w (-. h); Vec.create (-. w) h; Vec.create (-. w) (-. h)] in
-  List.map relative_corners  ~f:(fun corner -> Vec.add corner t.center)
+  let relative_corners =
+    [ Vec.create w h
+    ; Vec.create w (-.h)
+    ; Vec.create (-.w) h
+    ; Vec.create (-.w) (-.h)
+    ]
+  in
+  List.map relative_corners ~f:(fun corner -> Vec.add corner t.center)
