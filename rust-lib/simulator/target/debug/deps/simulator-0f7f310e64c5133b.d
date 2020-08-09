93be885caf5b68e5e43933e5ee7b0356c15b6b54
Yaron M. Minsky <yminsky@gmail.com>
2020-08-08 21:24:49 -0400

    remove some dead stuff

--- rust-lib/simulator/target/debug/deps/simulator-0f7f310e64c5133b.d
+++
@@ -1,7 +0,0 @@
-/Users/goobjar/Code/VirtualRobotMania/rust-lib/src/simulator/target/debug/deps/simulator-0f7f310e64c5133b.rmeta: src/lib.rs
-
-/Users/goobjar/Code/VirtualRobotMania/rust-lib/src/simulator/target/debug/deps/libsimulator-0f7f310e64c5133b.rlib: src/lib.rs
-
-/Users/goobjar/Code/VirtualRobotMania/rust-lib/src/simulator/target/debug/deps/simulator-0f7f310e64c5133b.d: src/lib.rs
-
-src/lib.rs:
--- rust-lib/simulator/target/debug/libsimulator.d
+++
@@ -1,1 +0,0 @@
-/Users/goobjar/Code/VirtualRobotMania/rust-lib/src/simulator/target/debug/libsimulator.rlib: /Users/goobjar/Code/VirtualRobotMania/rust-lib/src/simulator/src/lib.rs
