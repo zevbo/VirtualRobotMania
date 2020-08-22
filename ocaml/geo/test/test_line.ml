98c277ea343f61c3ff64154a3e32d9b0642fda2b
Yaron M. Minsky <yminsky@gmail.com>
2020-08-22 17:35:25 -0400

    more bugfixes

--- ocaml/geo/test/test_line.ml
+++ ocaml/geo/test/test_line.ml
@@ -15,22 +15,22 @@ let%expect_test "line and line-like" =
   [%expect
     {|
     ((base    (1 1))
-     (dir_vec (0 0))) |}];
+     (dir_vec (2 3))) |}];
   show (v (-1.) 1.) (v 1. (-1.));
   [%expect
     {|
     ((base    (-1 1))
-     (dir_vec (0  0))) |}];
+     (dir_vec (2  -2))) |}];
   show (v 1. (-1.)) (v (-1.) 1.);
   [%expect
     {|
-    ((base    (1 -1))
-     (dir_vec (0 0))) |}];
+    ((base    (1  -1))
+     (dir_vec (-2 2))) |}];
   show (v 3. (-4.)) (v 1. 0.);
   [%expect
     {|
-    ((base    (3 -4))
-     (dir_vec (0 0))) |}]
+    ((base    (3  -4))
+     (dir_vec (-2 4))) |}]
 
 let%expect_test "param" =
   let param ll p =
@@ -40,14 +40,14 @@ let%expect_test "param" =
   in
   let line = l (v 0. 0.) (v 10. 10.) in
   param line 0.;
-  [%expect {| ((pt (0 0)) (p' ())) |}];
+  [%expect {| ((pt (0 0)) (p' (0))) |}];
   param line 1.;
-  [%expect {| ((pt (0 0)) (p' ())) |}];
+  [%expect {| ((pt (10 10)) (p' (1))) |}];
   param line 0.5;
-  [%expect {| ((pt (0 0)) (p' ())) |}];
+  [%expect {| ((pt (5 5)) (p' (0.5))) |}];
   param line (-1.);
   (* BUG *)
-  [%expect {| ((pt (0 0)) (p' ())) |}]
+  [%expect {| ((pt (-10 -10)) (p' (-1))) |}]
 
 let%expect_test "on line" =
   let on_line ll pt =
@@ -59,24 +59,24 @@ let%expect_test "on line" =
   let line = l (v 0. 0.) (v 10. 10.) in
   on_line line (v 1. 1.);
   [%expect {|
-    ((result false)
-     (param  -NAN)) |}];
+    ((result true)
+     (param  0.1)) |}];
   on_line line (v 0. 0.);
   [%expect {|
-    ((result false)
-     (param  -NAN)) |}];
+    ((result true)
+     (param  0)) |}];
   on_line line (v (-1.) (-1.));
   [%expect {|
-    ((result false)
-     (param  -NAN)) |}];
+    ((result true)
+     (param  -0.1)) |}];
   on_line line (v 11. 11.);
   [%expect {|
-    ((result false)
-     (param  -NAN)) |}];
+    ((result true)
+     (param  1.1)) |}];
   on_line line (v 1. 3.);
   [%expect {|
     ((result false)
-     (param  -NAN)) |}];
+     (param  0.2)) |}];
   (* Now, a line with a different slope: (-2,4) *)
   let line = l (v 3. (-4.)) (v 1. 0.) in
   print_s
@@ -86,13 +86,13 @@ let%expect_test "on line" =
   print_s [%sexp (line : Line_like.line Line_like.t)];
   [%expect
     {|
-      ((base    (3 -4))
-       (dir_vec (0 0))) |}];
+      ((base    (3  -4))
+       (dir_vec (-2 4))) |}];
   (* BUG, seems like it handles the case of a non-uniform line wrong *)
   on_line line (v 2. (-2.));
   [%expect {|
-    ((result false)
-     (param  -NAN)) |}]
+    ((result true)
+     (param  0.5)) |}]
 
 let%expect_test "intersect" =
   let l1 = l (v (-1.) (-1.)) (v 1. 1.) in
@@ -100,4 +100,4 @@ let%expect_test "intersect" =
   let parallel = Line_like.are_parallel l1 l2 in
   let i = Line_like.intersection l1 l2 in
   print_s [%message "" (i : Vec.t option) (parallel : bool)];
-  [%expect {| ((i ()) (parallel false)) |}]
+  [%expect {| ((i ((0 0))) (parallel false)) |}]
