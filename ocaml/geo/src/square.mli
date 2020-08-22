00ce60b14a7e02f028c09ab697fbf88a64323095
zevbo <minsky-pz@bxscience.edu>
2020-08-22 19:39:36 -0400

    square.create

--- ocaml/geo/src/square.mli
+++ ocaml/geo/src/square.mli
@@ -3,7 +3,8 @@ type t =
   ; height : float
   ; center : Vec.t
   }
-  [@@deriving sexp_of]
+[@@deriving sexp_of]
 
+val create : float -> float -> Vec.t -> t
 val contains : t -> Vec.t -> bool
 val get_corners : t -> Vec.t list
