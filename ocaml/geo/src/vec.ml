ee5f7d5fbac14fc6853d89a31f3a3099ad2ae73d
zevbo <minsky-pz@bxscience.edu>
2020-08-21 22:18:22 -0400

    adding unit_vec

--- ocaml/geo/src/vec.ml
+++ ocaml/geo/src/vec.ml
@@ -69,3 +69,4 @@ let dot t1 t2 = Float.O.((t1.x * t2.x) + (t1.y * t2.y))
 let angle_of t = Float.atan2 t.y t.x
 let angle_between t1 t2 = angle_of (sub t2 t1)
 let angle_with_origin t1 t2 = normaize_angle (angle_of t1 -. angle_of t2)
+let unit_vec angle = rotate (create 1. 0.) angle
