6dd3f085b781f74859738453586b1e3dcddca45d
zevbo <minsky-pz@bxscience.edu>
2020-08-23 23:03:38 -0400

    tests

--- ocaml/virtuality2d/test/test_shape.ml
+++ ocaml/virtuality2d/test/test_shape.ml
@@ -14,7 +14,11 @@ let%expect_test _ =
   let tl = Vec.create (-100.) 100. in
   let bl = Vec.create (-100.) (-100.) in
   let br = Vec.create 100. (-100.) in
-  let s2 = Shape.create_closed [ tr; tl; bl; br ] (Material.create 1.0) in
+  let og = Vec.create 0. 0. in
+  let right = Vec.create 200. 0. in
+  let mat1 = Material.create 1.0 in
+  let s2 = Shape.create_closed [ tr; tl; bl; br ] mat1 in
+  let s3 = Shape.create [ Edge.create (Line_like.segment og right) mat1 ] in
   print_s [%sexp (s1 : Shape.t)];
   [%expect
     {|
@@ -27,4 +31,11 @@ let%expect_test _ =
   [%expect {|
     ((width  200)
      (height 200)
-     (center (0 0))) |}]
+     (center (0 0))) |}];
+  print_s [%sexp (s3.bounding_box : Rect.t)];
+  [%expect {|
+    ((width  200)
+     (height 0)
+     (center (100 0))) |}];
+  print_s [%sexp (List.length (Shape.intersections s2 s3) : int)];
+  [%expect {| 1 |}]
