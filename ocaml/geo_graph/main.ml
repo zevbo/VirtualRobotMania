24ca2abef90280970f439392bda7cca651e205f0
Yaron M. Minsky <yminsky@gmail.com>
2020-09-13 17:30:26 -0400

    some working graphics code, using OCaml stock graphics stuff

--- ocaml/geo_graph/main.ml
+++ ocaml/geo_graph/main.ml
@@ -1,30 +1,22 @@
-open Cairo
+open! Base
+open! Stdio
 
-let pi2 = 8. *. atan 1.
-
-let draw cr width height =
-  let r = 0.25 *. width in
-  set_source_rgba cr 0. 1. 0. 0.5;
-  arc cr (0.5 *. width) (0.35 *. height) ~r ~a1:0. ~a2:pi2;
-  fill cr;
-  set_source_rgba cr 1. 0. 0. 0.5;
-  arc cr (0.35 *. width) (0.65 *. height) ~r ~a1:0. ~a2:pi2;
-  fill cr;
-  set_source_rgba cr 0. 0. 1. 0.5;
-  arc cr (0.65 *. width) (0.65 *. height) ~r ~a1:0. ~a2:pi2;
-  fill cr
-
-let expose drawing_area _ev =
-  let cr = Cairo_gtk.create drawing_area#misc#window in
-  let allocation = drawing_area#misc#allocation in
-  draw cr (float allocation.Gtk.width) (float allocation.Gtk.height);
-  true
+exception Exit
 
 let () =
-  ignore (GMain.init () : string);
-  let w = GWindow.window ~title:"Gtk demo" ~width:500 ~height:400 () in
-  ignore (w#connect#destroy ~callback:GMain.quit : GtkSignal.id);
-  let d = GMisc.drawing_area ~packing:w#add () in
-  ignore (d#event#connect#expose ~callback:(expose d) : GtkSignal.id);
-  w#show ();
-  GMain.main ()
+  Graphics.open_graph "";
+  Graphics.resize_window 600 600;
+  Graphics.set_window_title "Testing, 1,2,3";
+  Graphics.auto_synchronize false;
+  try
+    for i = 0 to 1000 do
+      Graphics.clear_graph ();
+      if Graphics.key_pressed () then raise Exit;
+      Unix.sleepf 0.01;
+      let offset = Float.of_int i /. 0.1 |> Float.to_int in
+      printf "offset: %d\n%!" offset;
+      Graphics.fill_poly [| 0 + offset, 0 + offset; 100, 400; 400, 100 |];
+      Graphics.synchronize ()
+    done
+  with
+  | Exit -> ()
