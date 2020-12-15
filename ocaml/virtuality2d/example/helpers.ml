193b118cfdf6c6f0227fb347e619c87b9e5aab84
Yaron M. Minsky <yminsky@gmail.com>
2020-12-14 21:07:04 -0500

    added helpers and test, added time deduction

---
+++ ocaml/virtuality2d/example/helpers.ml
@@ -0,0 +1,40 @@
+open Core
+open Geo_graph
+open Tsdl
+
+let fps = 20.
+
+module State = struct
+  type t =
+    { event : Sdl.event
+    ; display : Display.t
+    }
+
+  let create () =
+    { event = Sdl.Event.create ()
+    ; display = Display.init ~w:1000 ~h:1000 ~title:"test"
+    }
+end
+
+let sim_loop (state : State.t) f =
+  while true do
+    let start = Time.now () in
+    if Sdl.poll_event (Some state.event)
+    then (
+      match Sdl.Event.enum (Sdl.Event.get state.event Sdl.Event.typ) with
+      | `Key_up ->
+        let key = Sdl.Event.get state.event Sdl.Event.keyboard_keycode in
+        printf "Key: %s\n%!" (Sdl.get_key_name key);
+        if key = Sdl.K.q then Caml.exit 0
+      | _ -> ());
+    Display.clear state.display Color.white;
+    f ();
+    Display.present state.display;
+    let time_spent_ms =
+      let stop = Time.now () in
+      Time.Span.to_ms (Time.diff stop start)
+    in
+    let target_delay_ms = 1000. /. fps in
+    let time_left_ms = Float.max 0. (target_delay_ms -. time_spent_ms) in
+    Sdl.delay (Int32.of_float time_left_ms)
+  done
