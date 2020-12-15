5ec390f1a7ceca7e06890fdf09a2f4dced25a956
zevbo <minsky-pz@bxscience.edu>
2020-12-14 20:25:16 -0500

    small test written

--- ocaml/virtuality2d/example/simple.ml
+++ ocaml/virtuality2d/example/simple.ml
@@ -1,6 +1,7 @@
 open Core_kernel
 open Virtuality2d
 open Geo_graph
+open Geo
 open Tsdl
 
 let fps = 20.
@@ -9,8 +10,9 @@ let main () =
   let elastic = Material.create ~energy_ret:1. in
   let shape = Shape.create_rect 10. 10. elastic in
   let robot = Body.create shape 1. 1. 5. in
+  let robot = Body.apply_com_impulse robot (Vec.create 2. 0.) in
   let display = Display.init ~w:500 ~h:500 ~title:"test" in
-  let _world = ref (World.of_bodies [ robot ]) in
+  let world = ref (World.of_bodies [ robot ]) in
   let image =
     Display.Image.of_bmp_file display "../../../images/test-robot.bmp"
   in
@@ -31,7 +33,7 @@ let main () =
       image
       robot.pos
       (Geo.Angle.of_radians robot.angle);
-    (*world := World.advance (!world) (1. /. fps);*)
+    world := World.advance (!world) (1. /. fps);
     Display.present display;
     Sdl.delay (Int32.of_float (1000. /. fps))
   done;
