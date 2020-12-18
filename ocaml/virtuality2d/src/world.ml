b99f2d7f4d0358ddfcf640dd265c913fc66eaa38
Yaron M. Minsky <yminsky@gmail.com>
2020-12-17 20:19:09 -0500

    conflict

--- ocaml/virtuality2d/src/world.ml
+++ ocaml/virtuality2d/src/world.ml
@@ -3,7 +3,6 @@ open! Base
 module Id : sig
   type t
 
-<<<<<<< HEAD
   include Comparable.S with type t := t
 
   val to_int : t -> int
@@ -63,24 +62,3 @@ let advance t ~dt =
     bodies =
       collide_bodies t.bodies |> Map.map ~f:(fun body -> Body.advance body ~dt)
   }
-=======
-let create () = { bodies = [] }
-let of_bodies bodies = { bodies }
-let add_body t body = { bodies = body :: t.bodies }
-
-(* TODO: If objects get stuck, we might want a small bounce *)
-let rec collide_bodies bodies =
-  match bodies with
-  | [] -> []
-  | first_body :: tl ->
-    (* I'm confused about this... *)
-    let new_first_body, new_tl =
-      List.fold_map tl ~init:first_body ~f:Body.collide
-    in
-    new_first_body :: collide_bodies new_tl
-
-let advance t dt =
-  let collided = collide_bodies t.bodies in
-  let restricted = List.map ~f:Body.apply_restrictions collided in
-  { bodies = List.map ~f:(fun body -> Body.advance body dt) restricted }
->>>>>>> master
