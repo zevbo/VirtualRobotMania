8ffbb8db487b6e2b166f0f2a99c849a0f2e5ab67
Yaron M. Minsky <yminsky@gmail.com>
2020-12-19 10:15:22 -0500

    basic bot up and running

--- ocaml/bindings/bindings.ml
+++ ocaml/bindings/bindings.ml
@@ -5,5 +5,7 @@ open Ctypes
 
 module Stubs (I : Cstubs_inverted.INTERNAL) = struct
   let () =
-    I.internal "double_int" (int @-> returning int) Robot_sim.Simple.double_int
+    I.internal "double_int" (int @-> returning int) Robot_sim.Simple.double_int;
+    I.internal "add_bot" (void @-> returning int) Robot_sim.Game.add_bot;
+    I.internal "step" (void @-> returning void) Robot_sim.Game.step
 end
