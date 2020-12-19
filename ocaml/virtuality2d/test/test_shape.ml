6d86828708bb0757181d157762cb6c3e8c72be76
Yaron M. Minsky <yminsky@gmail.com>
2020-12-18 21:22:22 -0500

    fixed test, I guess

--- ocaml/virtuality2d/test/test_shape.ml
+++ ocaml/virtuality2d/test/test_shape.ml
@@ -4,11 +4,13 @@ open Geo
 open Virtuality2d
 
 let%expect_test _ =
+  let mat1 = Material.create ~energy_ret:1.0 in
   let s1 =
-    { Shape.edges = []
-    ; bounding_box =
-        { width = 100.; height = 100.; center = Vec.create 10. 200. }
-    }
+    Shape.create_standard_rect
+      100.
+      100.
+      ~com:(Vec.create 10. 200.)
+      ~material:mat1
   in
   let tr = Vec.create 100. 100. in
   let tl = Vec.create (-100.) 100. in
@@ -16,17 +18,49 @@ let%expect_test _ =
   let br = Vec.create 100. (-100.) in
   let og = Vec.create 0. 0. in
   let right = Vec.create 200. 0. in
-  let mat1 = Material.create ~energy_ret:1.0 in
-  let s2 = Shape.create_closed [ tr; tl; bl; br ] mat1 in
-  let s3 = Shape.create [ Edge.create (Line_like.segment og right) mat1 ] in
+  let s2 =
+    Shape.create_closed
+      ~points:[ tr; tl; bl; br ]
+      ~material:mat1
+      ~average_r:75.
+      ~inertia_over_mass:100.
+  in
+  let s3 =
+    Shape.create
+      ~edges:[ Edge.create (Line_like.segment og right) mat1 ]
+      ~average_r:75.
+      ~inertia_over_mass:100.
+  in
   print_s [%sexp (s1 : Shape.t)];
   [%expect
     {|
-      ((edges ())
+      ((edges (
+         ((ls (
+            (base    (-10  -150))
+            (dir_vec (-100 0))
+            (flips   (0.5  -0.5))))
+          (material ((energy_ret 1))))
+         ((ls (
+            (base    (40  -200))
+            (dir_vec (0   100))
+            (flips   (0.5 -0.5))))
+          (material ((energy_ret 1))))
+         ((ls (
+            (base    (-10 -250))
+            (dir_vec (100 0))
+            (flips   (0.5 -0.5))))
+          (material ((energy_ret 1))))
+         ((ls (
+            (base    (-60 -200))
+            (dir_vec (0   100))
+            (flips   (0.5 -0.5))))
+          (material ((energy_ret 1))))))
        (bounding_box (
          (width  100)
          (height 100)
-         (center (10 200))))) |}];
+         (center (-10 -200))))
+       (average_r         NAN)
+       (inertia_over_mass 67066.666666666672)) |}];
   print_s [%sexp (s2.bounding_box : Rect.t)];
   [%expect {|
     ((width  200)
