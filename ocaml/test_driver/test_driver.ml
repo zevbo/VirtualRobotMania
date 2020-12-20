64677bd1c75ec64e6ed3a1fe741aa482545cc261
Yaron M. Minsky <yminsky@gmail.com>
2020-12-20 17:37:34 -0500

    small fixes to test driver

--- ocaml/test_driver/test_driver.ml
+++ ocaml/test_driver/test_driver.ml
@@ -75,8 +75,17 @@ let play_ctf client =
   let maybe p then_ else_ =
     if Float.O.(Random.float 1. < p) then force then_ else force else_
   in
+  let%bind () = dispatch Protocol.use_defense_bot () in
+  let%bind () = dispatch Protocol.set_motors (1., 1.) in
+  let%bind () = dispatch Protocol.use_offense_bot () in
   let%bind () = dispatch Protocol.set_motors (1., 1.) in
   let rec loop () =
+    let%bind () =
+      maybe
+        0.5
+        (lazy (dispatch Protocol.use_defense_bot ()))
+        (lazy (dispatch Protocol.use_offense_bot ()))
+    in
     let%bind () = dispatch Protocol.step () in
     let%bind () =
       maybe 0.01 (lazy (dispatch Protocol.shoot_laser ())) (lazy Deferred.unit)
