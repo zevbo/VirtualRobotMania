878ce0c867b2c7c206882be59cf37d9974485196
Yaron M. Minsky <yminsky@gmail.com>
2020-12-22 14:57:47 -0500

    implement degrees mode

--- manias/ctf/defense.rkt
+++ manias/ctf/defense.rkt
@@ -1,11 +1,19 @@
 #lang racket
 (require "../../racket/ctf/defense.rkt")
-(provide defense-bot)
+(provide (all-defined-out))
+
+(degrees-mode)
+;; uncomment the next line and comment out the above line if you want
+;; to work in radians.  Radians is a lot easier if you're going to do
+;; trigonometric functions like sin, cos, etc.
+;(radians mode)
+
+(define (on-tick tick-num)
+  (set-motors 1 -1)
+  (shoot-laser))
 
 (define defense-bot
   (make-robot
-   "Purple Defenders"
-   (lambda (tickno)
-     (set-motors 1 -1)
-     (shoot-laser))
+   "Defense"
+   on-tick
    #:body-color 'yellow))
--- manias/ctf/offense.rkt
+++ manias/ctf/offense.rkt
@@ -1,12 +1,20 @@
 #lang racket
 (require "../../racket/ctf/offense.rkt")
-(provide offense-bot)
+(provide (all-defined-out))
+
+(degrees-mode)
+;; uncomment the next line and comment out the above line if you want
+;; to work in radians.  Radians is a lot easier if you're going to do
+;; trigonometric functions like sin, cos, etc.
+;(radians mode)
+
+(define (on-tick tick-num)
+  (if (< (looking-dist 0) 400)
+      (set-motors -0.8 -1)
+      (set-motors 1 0.9)))
 
 (define offense-bot
   (make-robot
-   "Green offenders"
-   (lambda (tickno)
-     (if (< (looking-dist 0) 400)
-         (set-motors -0.8 -1)
-         (set-motors 1 0.9)))
+   "Offense"
+   on-tick
    #:body-color 'green))
