e98f9d63fd4c686f4b6eabc43aa17bdf06c9c6cc
zevbo <minsky-pz@bxscience.edu>
2020-12-21 20:51:59 -0500

    test

--- racket/ctf/driver.rkt
+++ racket/ctf/driver.rkt
@@ -1,19 +1,32 @@
 #lang racket
 (require 2htdp/image)
 (require "../engine-connect.rkt")
-;(require "../robotVisualization.rkt")
+(require "../robotVisualization.rkt")
 
-(define (use_defense_bot c) (rpc c "use_defense_bot"))
-(define (use_offense_bot c) (rpc c "use_offense_bot"))
-(define (step c) (rpc c "step"))
+(define (set-robot-image c defense? image file_path)
+  (define file (make-temporary-file file_path))
+  (println file)
+  (save-image image file)
+  (rpc c
+       (if defense?
+           `(#"set-defense-image"
+           (,(path->bytes file)))
+           `(#"set-offense-image"
+           (,(path->bytes file)))))
+  #;(delete-file file))
+(define (set_defense_bot c path) (rpc c '("use_defense_bot" p)))
+(define (set_offense_bot c path) (rpc c '("use_offense_bot" ())))
+(define (step c) (rpc c `(#"step" ())))
 
 (println (current-directory))
 (println "About to connect")
-(define c (launch-and-connect "test"))
+(define c (launch-and-connect "ctf"))
 (println "connected")
 
+(define image (create-robot-img "blue" "black" "Cashuu 2.0"))
+(define file "test-robot-~a.png")
+(set-robot-image c #t image file)
 (define (loop)
-  (println "step")
   (step c)
   (loop))
 (loop)
