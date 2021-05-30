3d554848527d869c7a64ad4e5fb5102f8229eb7b
Yaron M. Minsky <yminsky@gmail.com>
2021-05-29 21:59:52 -0400

    more fullsome example

--- ocaml/game_server_js/main.ml
+++ ocaml/game_server_js/main.ml
@@ -3,10 +3,17 @@ open! Async_kernel
 open Brr
 module Canvas = Brr_canvas.Canvas
 module C2d = Brr_canvas.C2d
+module Matrix4 = Brr_canvas.Matrix4
+
+let pelosi =
+  "https://cdn.britannica.com/s:250x250,c:crop/93/204193-050-16E326DA/Nancy-Pelosi-2018.jpg"
+
+let def fut =
+  Deferred.create (fun ivar -> Fut.await fut (fun x -> Ivar.fill ivar x))
 
 let () =
-  let w = 500
-  and h = 500 in
+  let w = 500 in
+  let h = 500 in
   let canvas = Brr_canvas.Canvas.create ~d:G.document ~w:500 ~h:500 [] in
   El.set_children (Document.body G.document) [ Canvas.to_el canvas ];
   let c2d = C2d.create canvas in
@@ -16,9 +23,19 @@ let () =
     C2d.stroke c2d path
   in
   Async_js.init ();
+  let img = El.img ~at:[ At.src (Jstr.of_string pelosi) ] () in
+  let img_src = C2d.image_src_of_el img in
   let rec loop n =
+    let img_w = El.prop El.Prop.width img |> Float.of_int in
+    let img_h = El.prop El.Prop.height img |> Float.of_int in
+    print_s [%message "dims" (img_w : float) (img_h : float)];
     C2d.clear_rect c2d ~x:0. ~y:0. ~w:(Float.of_int w) ~h:(Float.of_int h);
     let size = 200. *. (1. +. Float.sin (Float.of_int n /. 15.)) in
+    let theta = Float.of_int n *. Float.pi /. 100. in
+    C2d.translate c2d ~x:size ~y:(size /. 2.);
+    C2d.rotate c2d theta;
+    C2d.draw_image c2d img_src ~x:(-.(img_w /. 2.)) ~y:(-.(img_h /. 2.));
+    C2d.reset_transform c2d;
     draw_lines [ 0., 0.; 0., size; size, size; size, 0.; 0., 0. ];
     draw_lines [ 0., 0.; size, size ];
     draw_lines [ 0., size; size, 0. ];
