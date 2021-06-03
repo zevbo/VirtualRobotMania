b1df5ce8a27d6a6619de7cf5db25c1bda73ea721
Yaron M. Minsky <yminsky@gmail.com>
2021-06-03 18:49:53 -0400

    added a bunch of logging, fixed some bugs with IO

--- ocaml/csexp_rpc_js/io_utils.ml
+++ ocaml/csexp_rpc_js/io_utils.ml
@@ -12,42 +12,55 @@ let closed ws =
         (fun _ -> Ivar.fill closed ())
         (Websocket.as_target ws))
 
+let def fut =
+  Deferred.create (fun ivar -> Fut.await fut (fun x -> Ivar.fill ivar x))
+
 module Iobuf : sig
   type t
 
   val create : int -> t
-  val fill : t -> string -> unit
+  val fill : t -> Blob.t -> unit Deferred.t
 
   type consume_result =
     | Consumed
     | Not_enough_data
+  [@@deriving sexp]
 
   val maybe_consume : t -> bytes -> consume_result
   val compact : t -> unit
 end = struct
   type t =
-    { bytes : Bytes.t
+    { bytes : (Bytes.t[@sexp.opaque])
     ; mutable data_start : int
     ; mutable data_stop : int
     }
+  [@@deriving sexp]
 
   let create size = { bytes = Bytes.create size; data_start = 0; data_stop = 0 }
   let consumable t = t.data_stop - t.data_start
 
-  let fill t string =
-    Bytes.From_string.blito ~src:string ~dst:t.bytes ~dst_pos:t.data_start ();
-    t.data_stop <- t.data_stop + String.length string
+  let fill t blob =
+    match%bind def (Blob.text blob) with
+    | Error err ->
+      raise_s
+        [%message
+          "error fetching text from blob"
+            ~_:(Jstr.to_string (Jv.Error.message err) : string)]
+    | Ok text ->
+      let string = Jstr.to_string text in
+      Bytes.From_string.blito ~src:string ~dst:t.bytes ~dst_pos:t.data_start ();
+      t.data_stop <- t.data_stop + String.length string;
+      print_s [%message "fill" (String.length string : int) (t : t)];
+      return ()
 
   type consume_result =
     | Consumed
     | Not_enough_data
+  [@@deriving sexp]
 
   let maybe_consume t dst =
     let dst_len = Bytes.length dst in
-    let msg =
-      Printf.sprintf "dist, consumable: %n, %n" dst_len (consumable t)
-    in
-    print_s [%message msg];
+    print_s [%message "maybe_consume" (dst_len : int) (consumable t : int)];
     if consumable t < dst_len
     then Not_enough_data
     else (
@@ -66,14 +79,20 @@ let input_of_websocket ws closed =
   let module Buffer = Core_kernel.Buffer in
   let input_buf = Iobuf.create 10_000 in
   let input_arrived = Bvar.create () in
+  let seq = Sequencer.create () in
   Ev.listen
     Message.Ev.message
     (fun message ->
-      print_s [%message "received message"];
-      Bvar.broadcast input_arrived ();
-      let message = Ev.as_type message in
-      Iobuf.compact input_buf;
-      Iobuf.fill input_buf (Jstr.to_string (Message.Ev.data message : Jstr.t)))
+      don't_wait_for
+        (Throttle.enqueue seq (fun () ->
+             print_s [%message "received message"];
+             let message = Ev.as_type message in
+             Iobuf.compact input_buf;
+             let%bind () =
+               Iobuf.fill input_buf (Message.Ev.data message : Blob.t)
+             in
+             Bvar.broadcast input_arrived ();
+             return ())))
     (Websocket.as_target ws);
   let rec really_read () dst =
     print_s [%message "really read"];
@@ -106,5 +125,6 @@ let output_of_websocket ws closed =
       Websocket.send_string ws (Jstr.of_string (Bytes.to_string bytes)))
 
 let io_of_websocket ws =
+  print_s [%message (Jstr.to_string (Websocket.binary_type ws) : string)];
   let closed = closed ws in
   input_of_websocket ws closed, output_of_websocket ws closed
