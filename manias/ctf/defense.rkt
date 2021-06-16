4f5a4662ff485d7b057f555449c100cc7dfbb6b4
zevbo <minsky-pz@bxscience.edu>
2021-06-16 18:46:59 -0400

    goodbye old bots

--- manias/ctf/defense.rkt
+++ manias/ctf/defense.rkt
@@ -3,29 +3,9 @@
 (provide defense-bot)
 
 (degrees-mode)
-;; uncomment the next line and comment out the above line if you want
-;; to work in radians.  Radians is a lot easier if you're going to do
-;; trigonometric functions like sin, cos, etc.
-;(radians mode)
-
-(struct PID (p i d [last-error #:mutable] [output #:mutable]))
-(define (add-error pid error)
-  (set-PID-output! pid (+ (* (PID-p pid) error) (* (PID-d pid) (- error (PID-last-error pid)))))
-  (set-PID-last-error! pid error))
-(define (make-PID p i predict)
-  (PID p i (* predict p) 0.0 0.0))
-
-(define follow-pid (make-PID 0.08 0.0 10))
 
 (define (on-tick tick-num)
-  (add-error follow-pid (angle-to-opp))
-  (define control (PID-output follow-pid))
-  (define speed (- (/ (dist-to-opp) 200) 0.5))
-  (set-motors (- speed control) (+ speed control))
-  (cond
-    [(< (abs (angle-to-opp)) (* (if (= (offense-lives-left) (next-laser-power)) 3 (next-laser-power)) 5))
-     (shoot-laser)]
-    [(< (abs (angle-to-opp)) 25) (load-laser)]))
+  (set-motors 0 0))
 
 (define defense-bot
   (make-robot
--- manias/ctf/offense.rkt
+++ manias/ctf/offense.rkt
@@ -2,45 +2,9 @@
 (require "../../racket/ctf/offense.rkt")
 (provide offense-bot)
 
-(struct PID (p i d [last-error #:mutable] [output #:mutable]))
-(define (add-error pid error)
-  (set-PID-output! pid (+ (* (PID-p pid) error) (* (PID-d pid) (- error (PID-last-error pid)))))
-  (set-PID-last-error! pid error))
-(define (make-PID p i predict)
-  (PID p i (* predict p) 0.0 0.0))
-
-(define flag-pid (make-PID 0.01 0.0 10))
-(define return-pid (make-PID 0.01 0.0 30))
-
-(define (go-right)
-  (degrees-mode)
-  (cond 
-    [(> (get-robot-angle) 15) (set-motors 0.5 0.35)]
-    [else (set-motors 0.5 0.35)]))
-
 (define offense-bot
   (make-robot
    "Green offenders"
    (lambda (tick#)
-     (define pid-using (if (offense-has-flag?) return-pid flag-pid))
-     (define backwards? (and (offense-has-flag?) (< (abs (get-robot-angle)) 90)))
-     (define robot-angle (normalize-angle (+ (if backwards? 180 0) (get-robot-angle))))
-     (define error (if (offense-has-flag?) (normalize-angle (- 180 robot-angle)) (angle-to-flag)))
-     (cond
-       [(= (modulo tick# 5) 0) (printf "error: ~s~n" error)]) 
-     (add-error pid-using error)
-     (define control (PID-output pid-using))
-     #|(cond
-       [(< control -1.8) (set! control -1.8)]
-       [(> control 1.8) (set! control 1.8)])|#
-     (define default (if backwards? -1 1))
-     (define (do-stuff amount)
-       (cond
-         [(> amount 0)
-          (offense-has-flag?)
-          (do-stuff (- amount 1))]))
-     (do-stuff 5)
-     (set-motors (- default control) (+ default control))
-     (cond
-       [(opp-just-fired?) (boost)]))
+     (set-motors 0 0))
    #:body-color 'green))
