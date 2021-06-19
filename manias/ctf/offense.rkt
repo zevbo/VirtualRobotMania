cb0aaf4f4538a53bff33e76d2840c54524d72887
Yaron M. Minsky <yminsky@gmail.com>
2021-06-18 21:26:42 -0400

    radians mdoe

--- manias/ctf/offense.rkt
+++ manias/ctf/offense.rkt
@@ -3,6 +3,7 @@
 (require "../../racket/ctf/vector.rkt")
 (require "../../racket/ctf/ll.rkt")
 (provide offense-bot)
+(radians-mode)
 
 (define (set-motors-vec v)
   (set-motors (vec-x v) (vec-y v)))
@@ -11,14 +12,15 @@
   (vec (- x) x))
 
 (define (on-tick tick-num)
-  (define thresh (/ pi 2))
+  (define thresh (/ pi 10))
   (define angle (angle-to-flag))
+  (println (cons 'angle angle))
   (cond
     [(offense-has-flag?) (set-motors 0. 0.)]
     [(or (> angle thresh) (< angle (- thresh)))
      (set-motors-vec (vec-add
                       (vec 0.3 0.3)
-                      (turn-vec (/ angle 100))))]
+                      (turn-vec (/ angle 3))))]
     [else (set-motors 1. 1.)]
     ))
 
